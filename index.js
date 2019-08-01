c46e56f34761295aaff7450bdc8c2443b7851654
Carlos Giraldo <cgiraldo@expedia.com>
2019-08-01 18:01:05 -0400

    ✨ Selects the first `g` element in Google's SR

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 javascript: (async function () {
     /* Get all answers in a google search page, which have a "g" class. */
-    let elements = document.querySelectorAll(".g");
+    let elements = document.querySelector(".g");
 })();
