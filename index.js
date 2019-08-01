49aab887de02afe4e39c56ec9dff154ec8247bb3
Carlos Giraldo <cgiraldo@expedia.com>
2019-08-01 18:07:55 -0400

    ✨ Remove element found using class `.g`

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 javascript: (async function () {
     /* Get all answers in a google search page, which have a "g" class. */
     let element = document.querySelector(".g");
+    /* Remove element */
+    element.remove();
+
+    /* TODO: How to get and remove all `.g` elements? */
 })();
